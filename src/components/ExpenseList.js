1931469ddd5f0c76c95df813bec3ef7d00e72bd2
Mike Shutov <mikeshutov@gmail.com>
2018-12-04 06:25:17 -0500

    Final Changes

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,17 +4,25 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from './../selectors/expenses';
 
 export const ExpenseList = (props) => (
-    <div>
-        {
-            props.expenses.length === 0 ? (
-                <p>No Expenses</p>
-            ) :(
-                props.expenses.map ((expense,index) => {
-                    return <ExpenseListItem {...expense} key={index}/>;
-                })
-            )
-        }
-        
+    <div className = "content-container">
+        <div className = "list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-desktop">Expense</div>
+            <div className="show-for-desktop">Amount</div>
+        </div>
+        <div className = "list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item list-item__message">
+                        <p>No Expenses</p>
+                    </div>
+                ) :(
+                    props.expenses.map ((expense,index) => {
+                        return <ExpenseListItem {...expense} key={index}/>;
+                    })
+                )
+            }
+        </div>
     </div>
 );
 
